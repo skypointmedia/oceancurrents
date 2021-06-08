9ea42a978d23c0938bf037c0e6a31bed3e44ede0
skypointmedia <info@skypoint.media>
2021-06-08 14:28:13 -0400

    Force SSL

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const path = require('path');
 const app = express()
 const port = process.env.PORT || 3000 // Heroku will need the PORT environment variable
 
-app.use(express.static(path.join(__dirname, 'build')));
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+      return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+  }
+
+
+app.use(express.static(path.join(__dirname, 'build')),requireHTTPS);
 
 app.listen(port, () => console.log(`App is live on port ${port}!`))
